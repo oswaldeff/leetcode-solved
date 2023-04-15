ab66273fd5d3a2c35c7cc5f55ba60448f41ba071
owaldeff <tialem94@gmail.com>
2023-04-15 16:48:27 +0900

    fix: unsolved

--- validate-stack-sequences.js
+++ validate-stack-sequences.js
@@ -1,7 +1,7 @@
 info = {
   descriptionUrl:
-    'https://leetcode.com/problems/validate-stack-sequences/description/',
-  createdAt: '2023-04-14',
+    "https://leetcode.com/problems/validate-stack-sequences/description/",
+  createdAt: "2023-04-14",
   updatedAt: null,
 };
 
@@ -12,23 +12,46 @@ info = {
  */
 
 function validateStackSequences(pushed, popped) {
-  const pushedResult = [];
-  const poppedResult = [];
-
+  const pushedNow = [];
+  let result = true;
   let initHead = false;
-  while (pushed) {
-    let pushedHead = pushed.shift(); // shife는 지양
+
+  while (pushed.length > 0 || popped.length > 0) {
+    let pushedHead = pushed.shift();
     let poppedHead = popped.shift();
     if (!initHead && pushedHead === poppedHead) {
       initHead = true;
     } else if (!initHead && pushedHead !== poppedHead) {
-      pushedResult.push(pushedHead);
+      pushedNow.push(pushedHead);
       popped.unshift(poppedHead);
-    } else {
-      while (popped) {
-        // FIXME
+    } else if (initHead && pushedHead !== poppedHead) {
+      if (pushedNow.length > 0) {
+        pushedNowHead = pushedNow.pop();
+        if (pushedNowHead !== poppedHead) {
+          console.log("Nowhead: ", pushedNowHead);
+          pushedNow.push(pushedNowHead);
+        }
+        console.log("pushedNow With Length: ", pushedNow);
+      } else {
+        if (pushedHead !== undefined) {
+          console.log("head: ", pushedHead);
+          pushedNow.push(pushedHead);
+        }
       }
+    } else {
+      // initHead && pushedHead===poppedHead
     }
+    console.log("pushedNow: ", pushedNow);
   }
-  return true;
+  console.log("============");
+  console.log(pushed);
+  console.log(popped);
+  console.log("pushedNow", pushedNow);
+  console.log("length", pushedNow.length);
+
+  if (pushedNow.length !== 0) {
+    console.log("result is false");
+    result = false;
+  }
+  return result;
 }
