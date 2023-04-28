55c81e9085b7209530bbd230277f796c5a2cfb79
owaldeff <tialem94@gmail.com>
2023-04-28 14:03:27 +0900

    feat: solved

---
+++ bulb-switcher.js
@@ -0,0 +1,18 @@
+info = {
+  descriptionUrl: "https://leetcode.com/problems/bulb-switcher/description/",
+  createdAt: "2023-04-27",
+  updatedAt: null,
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+function bulbSwitch(n) {
+  const bulbs = Array.from({ length: n }, () => 1);
+  let i = 2;
+  while (i <= n) {
+    break;
+  }
+}
